6a074f8cb766bd2fb5a4d172c6cdfa24c1fb9a0a
Eusebiu Timofte <timoftesebi98@gmail.com>
2021-05-09 23:29:02 +0300

    done adding new marker and continuing to form activity

--- app/src/main/java/com/example/civichub/CustomMapActivity.kt
+++ app/src/main/java/com/example/civichub/CustomMapActivity.kt
@@ -2,9 +2,12 @@ package com.example.civichub
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.ScaleDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -12,9 +15,11 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -22,6 +27,8 @@ class CustomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnI
 
     private lateinit var mMap: GoogleMap
     private lateinit var jsonArray: JSONArray
+    private lateinit var fabAdd: FloatingActionButton
+    private lateinit var fabConfirmMarker: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +37,8 @@ class CustomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnI
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.customMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
+        fabAdd = findViewById(R.id.fabAddIssue)
+        fabConfirmMarker = findViewById(R.id.fabConfirmMarker)
     }
 
     /**
@@ -88,6 +96,23 @@ class CustomMapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnI
         val zoom = sharedPref.getFloat(getString(R.string.camera_position_zoom), (-100.0).toFloat())
         if (! (latitude == -100.0 || longitude == -100.0 || zoom == (-100.0).toFloat())){
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), zoom))
+        }
+
+        fabAdd.setOnClickListener {
+            val markerNew = mMap.addMarker(MarkerOptions().position(
+                LatLng(mMap.cameraPosition.target.latitude, mMap.cameraPosition.target.longitude)).
+            draggable(true).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_blue_marker)))
+            Toast.makeText(this, "Drag the blue marker where the issue is", Toast.LENGTH_LONG).show()
+            it.visibility = View.INVISIBLE
+            fabConfirmMarker.visibility = View.VISIBLE
+        }
+
+        fabConfirmMarker.setOnClickListener {
+            val intent = Intent(applicationContext, NewIssueFormActivity::class.java).apply {
+                putExtra("Latitude", mMap.cameraPosition.target.latitude)
+                putExtra("Logitude", mMap.cameraPosition.target.longitude)
+            }
+            startActivity(intent)
         }
     }
 
