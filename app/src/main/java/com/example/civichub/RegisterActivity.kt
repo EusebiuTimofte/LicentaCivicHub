19f2d03986198994ab10260747ed2a28fe84e924
Eusebiu Timofte <timoftesebi98@gmail.com>
2021-06-01 19:04:21 +0300

    design login and register done

--- app/src/main/java/com/example/civichub/RegisterActivity.kt
+++ app/src/main/java/com/example/civichub/RegisterActivity.kt
@@ -71,9 +71,11 @@ class RegisterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
             this,
             R.array.planets_array,
             android.R.layout.simple_spinner_item
+//            R.layout.custom_spinner_item
         ).also { adapter ->
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item)
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
